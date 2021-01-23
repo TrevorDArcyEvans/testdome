6eac0baf105575c5c4ed908b66cfe8d5e5a85e39
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-01-23 17:21:22 +0000

    - more problem solutions

---
+++ Hobbies.cs
@@ -0,0 +1,33 @@
+﻿namespace TestDome
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public sealed class Hobbies
+	{
+		private readonly Dictionary<string, string[]> _hobbies = new Dictionary<string, string[]>();
+
+		public void Add(string hobbyist, params string[] hobbies)
+		{
+			_hobbies.Add(hobbyist, hobbies);
+		}
+
+		public List<string> FindAllHobbyists(string hobby)
+		{
+			return (from kvp in _hobbies where kvp.Value.Contains(hobby) select kvp.Key).ToList();
+		}
+
+#if false
+		public static void Main(string[] args)
+		{
+			var hobbies = new Hobbies();
+			hobbies.Add("Steve", "Fashion", "Piano", "Reading");
+			hobbies.Add("Patty", "Drama", "Magic", "Pets");
+			hobbies.Add("Chad", "Puzzles", "Pets", "Yoga");
+
+			hobbies.FindAllHobbyists("Yoga").ForEach(Console.WriteLine);
+		}
+#endif
+	}
+}
---
+++ WeightedAverage.cs
@@ -0,0 +1,41 @@
+﻿namespace TestDome
+{
+	using System;
+	using System.Collections.Generic;
+
+	public sealed class WeightedAverage
+	{
+		public static double Mean(IList<int> numbers, IList<int> weights)
+		{
+			if (numbers == null || weights == null)
+			{
+				throw new ArgumentException("null parameter");
+			}
+
+			if (numbers.Count != weights.Count)
+			{
+				throw new ArgumentException("length mismatch");
+			}
+
+			long total = 0;
+			long totalWeights = 0;
+			for (var i = 0; i < numbers.Count; i++)
+			{
+				total += numbers[i] * weights[i];
+				totalWeights += weights[i];
+			}
+
+			return total / (double)totalWeights;
+		}
+
+#if false
+		public static void Main(string[] args)
+		{
+			var values = new[] { 3, 6 };
+			var weights = new[] { 4, 2 };
+
+			Console.WriteLine(WeightedAverage.Mean(values, weights));
+		}
+#endif
+	}
+}
